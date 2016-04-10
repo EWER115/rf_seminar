df11bbe113b926a6752c094464df8d46f4824c9a
EWER115 <stepec.dejan93@gmail.com>
2016-04-10 04:38:26 +0200

    bug fix

--- code/traceSeam.m
+++ code/traceSeam.m
@@ -5,18 +5,32 @@ function [T] = traceSeam(C)
     for i = size(C,1)-1:-1:1
       if T(i+1) == 1
           [~,idx] = min([C(i,T(i+1)),C(i,T(i+1)+1)]);
+          
+          if idx == 1
+              T(i) = T(i+1);
+          else
+              T(i) = min(size(C, 2), T(i+1)+1);
+          end
+          
       elseif T(i+1) < size(C,2) && T(i+1) > 1
           [~,idx] = min([C(i,T(i+1)-1),C(i,T(i+1)),C(i,T(i+1)+1)]);
+          
+          if idx == 1
+              T(i) = max(1, T(i+1)-1);
+          elseif idx == 2
+              T(i) = T(i+1);
+          else
+              T(i) = min(size(C, 2), T(i+1)+1);
+          end
+          
       else
           [~,idx] = min([C(i,T(i+1)-1),C(i,T(i+1))]);
-      end
-      
-      if idx == 1
-          T(i) = max(1, T(i+1) - 1);
-      elseif idx == 2
-          T(i) = T(i+1);
-      else
-          T(i) = min(size(C, 2), T(i+1)+1);
+          
+          if idx == 1
+              T(i) = max(1, T(i+1-1));
+          else
+              T(i) = T(i+1);
+          end
       end
     end
 end
