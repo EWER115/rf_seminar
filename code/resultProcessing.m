d81eef73be102886af26cee709bba1f11040416d
Anže Medved <anzze.medved@gmail.com>
2016-04-22 10:32:53 +0200

    Added scaled random data and train data, that was merged from all scales. To test accuracy of this model, I also scaled random data (refer to train_complete and random_complete).

--- code/resultProcessing.m
+++ code/resultProcessing.m
@@ -7,6 +7,9 @@
 [train_scales, train_labels_scales, test_scales, test_labels_scales] = ...
     loadTrainTestScales('../datalibsvm');
 
+% load random set
+[labRand, mtrxRand] = libsvmread('/home/anze/Faks/GitRepos/forenzika/rf_seminar/datalibsvm/test_features_random.lsvm');
+
 %% Create new files that include original data and scaled data
 % mask = '../datalibsvm/test_mixed_%04d.lsvm';
 % scales = [1 4 7 10 13 16 20 23 26 30];
@@ -16,6 +19,28 @@
 %     libsvmwrite(sprintf(mask, scales(i)), lbls, mtrx);
 % end
 
+% % Add some unmodified images to random set
+% newRandlbls = [labRand(1:180) ; test_labels_original(181:end)];
+% newMtrxRand = [mtrxRand(1:180,:); test_original(181:end,:)]; 
+% libsvmwrite('/home/anze/Faks/GitRepos/forenzika/rf_seminar/datalibsvm/test_random.lsvm', newRandlbls, newMtrxRand);
+
+% mask = '../datalibsvm/train_mixed_%04d.lsvm';
+% scales = [1 4 7 10 13 16 20 23 26 30];
+% crr = 0;
+% lbls = []; mtrx = [];
+% for i = 1 : length(scales)
+%     idx1 = crr + 1;
+%     crr = crr + 55;
+%     idx2 = crr;
+%     lbls = [lbls; train_labels_scales(idx1:idx2,i)];
+%     mtrx = [mtrx; train_scales{i}(idx1:idx2,:)];
+%     
+% end
+% 
+% lbls = [lbls ; train_labels_original(551:1000)];
+% mtrx = [mtrx ; train_original(551:1000,:)];
+% 
+% libsvmwrite('/home/anze/Faks/GitRepos/forenzika/rf_seminar/datalibsvm/train_complete.lsvm', lbls, mtrx);
 
 %% Optimize parameters
 % % read training set for parameter optimization
@@ -61,14 +86,15 @@
 % xlabel('c'), ylabel('Accuracy'); title('Accuracy vs. cost');
 
 %% Test for each scale
-maskTrain = '../datascaled2/train_%04d.lsvm';
-maskTest = '../datascaled2/test_%04d.lsvm';
-scales = [1 4 7 10 13 16 20 23 26 30];
-
- for i = 1 : length(test_scales)
-    [labelsT, mtrxT] = libsvmread(sprintf(maskTrain, scales(i)));
-    model = svmtrain(labelsT,mtrxT);
-    [labels, mtrx] = libsvmread(sprintf(maskTest, scales(i)));
-    [lbl, acc, dec] = svmpredict(labels,mtrx,model);
- end
+% maskTrain = '../datascaled2/train_%04d.lsvm';
+% maskTest = '../datascaled2/test_%04d.lsvm';
+% maskRandom = '../datascaled2/random_%04d.lsvm';
+% scales = [1 4 7 10 13 16 20 23 26 30];
+% 
+%  for i = 1 : length(test_scales)
+%     [labelsT, mtrxT] = libsvmread(sprintf(maskTrain, scales(i)));
+%     model = svmtrain(labelsT,mtrxT);
+%     [labels, mtrx] = libsvmread(sprintf(maskTest, scales(i)));
+%     [lbl, acc, dec] = svmpredict(labels,mtrx,model);
+%  end
 
